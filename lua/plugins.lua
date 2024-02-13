409ca93ca824288e4bfc106ff1d24a32565d6a33
Luis Natera <natera@hey.com>
2024-02-13 16:41:16 +0100

    Split config wip

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,50 +1,6 @@
 return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',
-        tag = "legacy",
-        event = "LspAttach",
-        opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-  },
-
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
-
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
@@ -54,87 +10,21 @@ return {
       show_trailing_blankline_indent = false,
     },
   },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
-
-
-  { -- vim-matchup for higlighting
-  "andymass/vim-matchup",
-  lazy = false,
-  enabled = true,
-  init = function()
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
-  end,
+-- vim-matchup for higlighting
+  {
+    "andymass/vim-matchup",
+    lazy = false,
+    enabled = true,
+    init = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
   },
-
-  { -- tmux navigation
-    "aserowy/tmux.nvim",
+-- tmux navigation
+  {     "aserowy/tmux.nvim",
     config = function() return require("tmux").setup() end
   },
-
-  { -- GitHub copilot
-    "github/copilot.vim",
-    init = function()
-      -- vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-    end,
-    config = function()
-      vim.keymap.set('i', '<C-e>', [[copilot#Accept("\<CR>")]], {
-        silent = true,
-        expr = true,
-        script = true,
-        replace_keycodes = false,
-      })
-    end,
-    },
-  { -- obsidian.nvim
-      "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "notes",
-        path = "~/notes",
-      },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
-    },
-    -- see below for full list of options 👇
-  },
-  },
-
-  {{ -- nvim-tree for file navigation
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-  },
-
   -- Glow
   {"ellisonleao/glow.nvim", config = true, cmd = "Glow"}
-}
 }
